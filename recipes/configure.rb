5c9741425eae728debcf0463a7ba2fdb8cd00b62
Tamby Kojak <tamby@hirewire.com>
2016-09-05 22:23:56 -0400

    Update configure.rb

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,4 +1,4 @@
-if node[:opsworks][:instance][:layers].include?('rails-app')
+if node[:opsworks][:instance][:layers].include?('rails-app') or node[:opsworks][:instance][:layers].include?('tools')
 
   include_recipe "opsworks_custom_env::restart_command"
   include_recipe "opsworks_custom_env::write_config"
